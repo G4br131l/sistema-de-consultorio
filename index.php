2d2d3153837a1417eca0f92ddf40da2874fb8e09
Gabriel <gabriel25ribeiro@gmail.com>
2024-03-01 21:18:14 -0300

    iniciando projeto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <div>
+        <h1>Entrar:</h1>
+        <form action="menuUsuario.php" method="post">
+            <label for="">Usuario:</label><br>
+            <input type="text" name="usuario" id=""><br>
+            <label for="">Senha:</label><br>
+            <input type="password" name="senha" id=""><br>
+            <input type="submit" value="Entrar">
+        </form>
+    </div><br>
+    <a href="criarConta.php">não tenho conta</a>
+</body>
+</html>
---
+++ menuUsuario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    <header>
+        <h1>Menu de usuario</h1>
+    </header>
+    <div>
+        <a href="cadastroConsulta.php">solicitar consulta</a>
+
+    </div>
+    <div>
+        <h2>Consultas solicitadas:</h2>
+        <?php ?>
+        <h2>Consultas respondidas:</h2>
+        <?php ?>
+    </div>
+</body>
+</html>
